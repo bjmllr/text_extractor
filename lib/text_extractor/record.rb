2c74df7622f5e5852560f43cf59bf315f184dc20
Ben Miller <bmiller@rackspace.com>
2016-03-17 00:06:21 -0400

    move factory analysis to inner class

--- lib/text_extractor/record.rb
+++ lib/text_extractor/record.rb
@@ -1,10 +1,11 @@
 class TextExtractor
   class Record
-    attr_reader :regexp, :values
+    attr_reader :regexp, :factory, :values
 
     def initialize(regexp, factory: nil, values: [], fill: [])
       @regexp = regexp
-      @constructor = analyze_factory(factory)
+      @factory = factory
+      @constructor = FactoryAnalyzer.new(factory).to_proc
       @values = values.map { |val| [val.id, val] }.to_h
       @default_values = values.map { |val| [val.id, nil] }.to_h
       @fill = Array(fill)
@@ -15,46 +16,6 @@ class TextExtractor
                     .merge!(extract_fills fill)
                     .merge!(extract_values match)
       build_extraction(extracted)
-    end
-
-    def analyze_factory(factory)
-      case factory
-      when Hash
-        analyze_factory_explicit(factory)
-      when Class
-        analyze_factory_implicit(factory)
-      end
-    end
-
-    def analyze_factory_explicit(factory)
-      klass, params = factory.first
-      case params
-      when Array
-        analyze_factory_explicit_positional(klass, params)
-      when Set
-        analyze_factory_explicit_keyword(klass, params)
-      end
-    end
-
-    def analyze_factory_explicit_positional(klass, params)
-      ->(extracted) { klass.new(*extracted.values_at(*params)) }
-    end
-
-    def analyze_factory_explicit_keyword(klass, params)
-      lambda do |extracted|
-        values = params.each_with_object({}) do |param, hash|
-          hash[param] = extracted[param]
-        end
-        klass.new(**values)
-      end
-    end
-
-    def analyze_factory_implicit(factory)
-      if factory.ancestors.include?(Struct)
-        ->(extracted) { factory.new(*extracted.values) }
-      else
-        ->(extracted) { factory.new(**extracted) }
-      end
     end
 
     def build_extraction(extracted)
@@ -81,5 +42,57 @@ class TextExtractor
     def extract_values(match)
       values.keys.map { |id| [id, values[id].convert(match[id])] }.to_h
     end
+
+    # converts the value of the factory option to a constructor proc
+    class FactoryAnalyzer
+      def initialize(factory)
+        case factory
+        when Hash
+          @klass, @params = factory.first
+        else
+          @klass = factory
+        end
+      end
+
+      def to_proc
+        if @params
+          explicit
+        elsif @klass
+          implicit
+        end
+      end
+
+      private
+
+      def explicit
+        case @params
+        when Array
+          positional
+        when Set
+          keyword
+        end
+      end
+
+      def positional
+        ->(extracted) { @klass.new(*extracted.values_at(*@params)) }
+      end
+
+      def keyword
+        lambda do |extracted|
+          values = @params.each_with_object({}) do |param, hash|
+            hash[param] = extracted[param]
+          end
+          @klass.new(**values)
+        end
+      end
+
+      def implicit
+        if @klass.ancestors.include?(Struct)
+          ->(extracted) { @klass.new(*extracted.values) }
+        else
+          ->(extracted) { @klass.new(**extracted) }
+        end
+      end
+    end # class FactoryAnalyzer
   end # class Record
 end # class TextExtractor
